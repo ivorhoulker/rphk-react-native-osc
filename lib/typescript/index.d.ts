6119142cb1ed4fc11cccdfc903d438a800cffe1a
Ivor Houlker <ivor.houlker@gmail.com>
2022-12-09 11:32:19 +0800

    ts fixes

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,26 +1,29 @@
-declare const Osc: any;
-/** This creates and starts the client - it doesn't return anything, just have to assume the client exists in native code
- *  @param serverAddress - your osc server's IP address
- *  @param serverPort - your osc server's listen port
- */
-export declare function createClient(serverAddress: string, serverPort: number): any;
-/** This creates and starts the server - it doesn't return anything, just have to assume the server exists in native code. It will bind to all interfaces.
- *  @param serverPort - the port you want to listen on.
- */
-export declare function createServer(serverPort: number): any;
-/** Send an osc message, which looks like: "/step/on/it", [1, 2, 3]
- * @param slashAddress - you must send any string content in the slash address, e.g. /jim/bob
- * @param content - an array of floats, ints, or booleans
- */
-export declare function sendMessage(slashAddress: string, content: Array<number | boolean>): any;
-/** Add a listener
- * @param eventType - the package only emits the "GotMessage" event
- * @param cb - a callback to handle osc data, that looks like {"/step/on/it", [1, 2, 3]}
- */
-export declare function addListener(eventType: 'GotMessage', cb: (d: {
+import { NativeModule } from 'react-native';
+export type OSCMessage = {
     address: string;
     data: Array<number | boolean>;
-}) => void): any;
-export declare function removeListeners(count: number): any;
-export default Osc;
+};
+interface OscModule extends NativeModule {
+    /** Add a listener for the "GotMessage" event
+     *  @param eventType - should be "GotMessage"
+   
+     */
+    addListener: (eventType: string) => void;
+    removeListeners: (count: number) => void;
+    /** This creates and starts the client - it doesn't return anything, just have to assume the client exists in native code
+     *  @param serverAddress - your osc server's IP address
+     *  @param serverPort - your osc server's listen port
+     */
+    createClient: (serverAddress: string, serverPort: number) => void;
+    /** This creates and starts the server - it doesn't return anything, just have to assume the server exists in native code. It will bind to all interfaces.
+     *  @param serverPort - the port you want to listen on.
+     */
+    createServer: (serverPort: number) => void;
+    /** Send an osc message, which looks like: "/step/on/it", [1, 2, 3]
+     * @param message - with an address and data
+     */
+    sendMessage: (message: OSCMessage) => void;
+}
+declare const _default: OscModule;
+export default _default;
 //# sourceMappingURL=index.d.ts.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModule, NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
   `The package '@rphk/react-native-osc' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,35 +17,27 @@ const Osc = NativeModules.Osc
       },
     );
 export type OSCMessage = { address: string; data: Array<number | boolean> };
-const NativeOsc = {
+
+interface OscModule extends NativeModule {
+  /** Add a listener for the "GotMessage" event
+   *  @param eventType - should be "GotMessage"
+ 
+   */
+  addListener: (eventType: string) => void;
+  removeListeners: (count: number) => void;
   /** This creates and starts the client - it doesn't return anything, just have to assume the client exists in native code
    *  @param serverAddress - your osc server's IP address
    *  @param serverPort - your osc server's listen port
    */
-  createClient(serverAddress: string, serverPort: number) {
-    return Osc.createClient(serverAddress, serverPort);
-  },
+  createClient: (serverAddress: string, serverPort: number) => void;
   /** This creates and starts the server - it doesn't return anything, just have to assume the server exists in native code. It will bind to all interfaces.
    *  @param serverPort - the port you want to listen on.
    */
-  createServer(serverPort: number) {
-    return Osc.createServer(serverPort);
-  },
+  createServer: (serverPort: number) => void;
   /** Send an osc message, which looks like: "/step/on/it", [1, 2, 3]
    * @param message - with an address and data
    */
-  sendMessage(message: OSCMessage) {
-    return Osc.sendMessage(message.address, message.data);
-  },
-  didReceive(message: OSCMessage) {
-    return Osc.didReceive(message);
-  },
-  supportedEvents(): string {
-    return Osc.supportedEvents();
-  },
-  requiresMainQueueSetup(): boolean {
-    return Osc.requiresMainQueueSetup();
-  },
-};
+  sendMessage: (message: OSCMessage) => void;
+}
 
-export default NativeOsc;
+export default Osc as OscModule;
